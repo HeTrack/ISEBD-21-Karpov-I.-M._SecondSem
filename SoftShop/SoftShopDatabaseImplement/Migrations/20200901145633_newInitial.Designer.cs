b59ff82d0a58de0bebf2a73bb3b900a6751b50ba
iliya.utd2014 <iliya.utd2014@gmail.com>
2020-09-01 18:57:57 +0400

    FIX

--- SoftShop/SoftShopDatabaseImplement/Migrations/20200901145633_newInitial.Designer.cs
+++ SoftShop/SoftShopDatabaseImplement/Migrations/20200901145633_newInitial.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SoftShopDatabaseImplement;
 
 namespace SoftShopDatabaseImplement.Migrations
 {
     [DbContext(typeof(SoftShopDatabase))]
-    partial class SoftShopDatabaseModelSnapshot : ModelSnapshot
+    [Migration("20200901145633_newInitial")]
+    partial class newInitial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SoftShop/SoftShopDatabaseImplement/Migrations/20200901145633_newInitial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SoftShopDatabaseImplement.Migrations
+{
+    public partial class newInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
